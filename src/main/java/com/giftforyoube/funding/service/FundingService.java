c2ffb03f855ec44b1782a4af5e80b680e8bfe93f
hyun <kingmandoo95@gmail.com>
2024-02-05 20:52:54 +0900

    fix : cachemanager 펀딩 수정, 삭제 적용

--- src/main/java/com/giftforyoube/funding/service/FundingService.java
+++ src/main/java/com/giftforyoube/funding/service/FundingService.java
@@ -189,7 +189,7 @@ public class FundingService {
 
     // 펀딩 수정
     @Transactional
-    @CacheEvict(value = {"activeFundings", "finishedFundings", "fundingDetail"}, allEntries = true)
+    @CacheEvict(value = {"activeFundings", "finishedFundings", "fundingDetail"}, cacheManager = "cacheManager", allEntries = true)
     public FundingResponseDto updateFunding(Long fundingId, User user, FundingCreateRequestDto requestDto) {
         // 펀딩 id 유효성검사
         Funding funding = fundingRepository.findById(fundingId).orElseThrow(
@@ -209,7 +209,7 @@ public class FundingService {
 
     // 펀딩 삭제
     @Transactional
-    @CacheEvict(value = {"activeFundings", "finishedFundings", "fundingDetail"}, allEntries = true)
+    @CacheEvict(value = {"activeFundings", "finishedFundings", "fundingDetail"}, cacheManager = "cacheManager", allEntries = true)
     public void deleteFunding(Long fundingId, User user) {
         // 펀딩 id 유효성 검사
         Funding funding = fundingRepository.findById(fundingId).orElseThrow(
