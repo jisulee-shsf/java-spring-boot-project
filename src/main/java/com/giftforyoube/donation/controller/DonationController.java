414a73c70656b4f81034b8ec5ad675467510bfa4
Jisu Lee <jisulee.shsf@gmail.com>
2024-02-05 17:11:53 +0900

    Feat: Kakaopay 단건 결제준비 기능 구현

--- src/main/java/com/giftforyoube/donation/controller/DonationController.java
+++ src/main/java/com/giftforyoube/donation/controller/DonationController.java
@@ -1,9 +1,31 @@
 package com.giftforyoube.donation.controller;
 
-import lombok.RequiredArgsConstructor;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.giftforyoube.donation.dto.ReadyDonationRequestDto;
+import com.giftforyoube.donation.dto.ReadyDonationResponseDto;
+import com.giftforyoube.donation.service.DonationService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequiredArgsConstructor
+@RequestMapping("/api")
 public class DonationController {
+
+    private String tid;
+    private final DonationService donationService;
+
+    public DonationController(DonationService donationService) {
+        this.donationService = donationService;
+    }
+
+    @PostMapping("/donation/ready")
+    public ResponseEntity readyDonation(@RequestBody ReadyDonationRequestDto readyDonationRequestDto) throws JsonProcessingException {
+        ReadyDonationResponseDto readyDonationResponseDto = donationService.readyDonation(readyDonationRequestDto);
+        tid = readyDonationResponseDto.getTid();
+        return new ResponseEntity<>(readyDonationResponseDto, HttpStatus.OK);
+    }
 }
