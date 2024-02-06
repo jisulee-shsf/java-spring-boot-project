e7e4edf3b3395bc7517efddd9d75cbf088feca36
Jisu Lee <jisulee.shsf@gmail.com>
2024-02-06 12:07:38 +0900

    Fix: CORS 설정 변경

--- src/main/java/com/giftforyoube/global/config/CorsConfig.java
+++ src/main/java/com/giftforyoube/global/config/CorsConfig.java
@@ -6,6 +6,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.util.List;
+
 @Configuration
 public class CorsConfig {
     @Bean
@@ -13,14 +15,14 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-
-        config.addAllowedOrigin("http://localhost:3001");
-        config.addAllowedOrigin("http://localhost:3000");
+        config.setAllowedOrigins(List.of(
+                "https://www.giftipie.me", "https://giftipie.me", "http://api.giftipie.me", "https://api.giftipie.me",
+                "http://localhost:3000", "http://localhost:3001",
+                "https://giftipie-vercel-qbjbn6707-torongs-projects.vercel.app/"));
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         config.addExposedHeader("*");
         config.setMaxAge(3600L);
-
         source.registerCorsConfiguration("/api/**", config);
         return new CorsFilter(source);
     }
