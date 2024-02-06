2229c7d7ef7e48a4f72ac01e51ac5d58c46b0146
LyricZen <kohoon0129@gmail.com>
2024-02-06 16:49:26 +0900

    style: Funding 리팩토링 (페이지네이션)

--- src/main/java/com/giftforyoube/global/config/RedisConfig.java
+++ src/main/java/com/giftforyoube/global/config/RedisConfig.java
@@ -1,17 +1,26 @@
 package com.giftforyoube.global.config;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.data.redis.serializer.*;
 
 @Configuration
+@EnableCaching
 public class RedisConfig {
 
     @Value("${spring.data.redis.host}")
@@ -20,7 +29,6 @@ public class RedisConfig {
     @Value("${spring.data.redis.port}")
     private int port;
 
-    // Redisson 으로 분산락 구현을 위해 redisson 설정 추가
     @Bean
     public RedissonClient redissonClient() {
         Config config = new Config();
@@ -30,16 +38,58 @@ public class RedisConfig {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(host, port);
-        return lettuceConnectionFactory;
+        return new LettuceConnectionFactory(host, port);
     }
 
     @Bean
-    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        RedisTemplate<String, String> template = new RedisTemplate<>();
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(redisConnectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         return template;
+    }
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration cacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(customRedisSerializer()));
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(cacheConfiguration)
+                .build();
+    }
+
+    // ObjectMapper의 setObjectMapper가 스프링부트 3.0 이상부터 deprecated되어
+    // 날짜 정보를 직렬화,역직렬화 하기위해 커스텀한 serializer를 사용
+    @Bean
+    public RedisSerializer<Object> customRedisSerializer() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule()); // JavaTimeModule 등록
+        objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
+
+        return new RedisSerializer<Object>() {
+            @Override
+            public byte[] serialize(Object t) throws SerializationException {
+                try {
+                    return objectMapper.writeValueAsBytes(t);
+                } catch (Exception e) {
+                    throw new SerializationException("Error serializing object to JSON", e);
+                }
+            }
+
+            @Override
+            public Object deserialize(byte[] bytes) throws SerializationException {
+                if (bytes == null || bytes.length == 0) {
+                    return null;
+                }
+                try {
+                    return objectMapper.readValue(bytes, Object.class);
+                } catch (Exception e) {
+                    throw new SerializationException("Error deserializing object from JSON", e);
+                }
+            }
+        };
     }
 }
