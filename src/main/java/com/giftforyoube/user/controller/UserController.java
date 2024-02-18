c5e466d28239b55dec0f9638bca3485f90a499c3
Jisu Lee <jisulee.shsf@gmail.com>
2024-02-19 04:08:14 +0900

    Fix: redirect 방법 재변경(중간 발표용)

--- src/main/java/com/giftforyoube/user/controller/UserController.java
+++ src/main/java/com/giftforyoube/user/controller/UserController.java
@@ -14,6 +14,7 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -22,6 +23,8 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Slf4j
 @RestController
@@ -58,8 +61,8 @@ public class UserController {
 
     // 3-1. 카카오 로그인
     @GetMapping("/kakao/callback")
-    public ResponseEntity<BaseResponse<String>> kakaoLogin(@RequestParam String code,
-                                                           HttpServletResponse httpServletResponse) throws JsonProcessingException, UnsupportedEncodingException {
+    public ResponseEntity<String> kakaoLogin(@RequestParam String code,
+                                             HttpServletResponse httpServletResponse) throws JsonProcessingException, UnsupportedEncodingException, URISyntaxException {
         String kakaoToken = kakaoService.kakaoLogin(code);
         Cookie cookie = new Cookie(JwtUtil.AUTHORIZATION_HEADER, kakaoToken);
         cookie.setPath("/");
@@ -67,14 +70,15 @@ public class UserController {
         cookie.setSecure(true);
         httpServletResponse.addCookie(cookie);
 
-        BaseResponse<String> baseResponse = new BaseResponse<>(BaseResponseStatus.KAKAO_LOGIN_SUCCESS, kakaoToken);
-        return ResponseEntity.status(HttpStatus.OK).body(baseResponse);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(new URI("https://www.giftipie.me/"));
+        return new ResponseEntity<>(headers, HttpStatus.FOUND);
     }
 
-    // 3-2. Google 로그인
+    // 3-2. 구글 로그인
     @GetMapping("/login/oauth2/code/google")
-    public ResponseEntity<BaseResponse<String>> googleLogin(@RequestParam String code,
-                                                            HttpServletResponse httpServletResponse) throws JsonProcessingException, UnsupportedEncodingException {
+    public ResponseEntity<String> googleLogin(@RequestParam String code,
+                                              HttpServletResponse httpServletResponse) throws JsonProcessingException, UnsupportedEncodingException, URISyntaxException {
         String googleToken = googleService.googleLogin(code);
         Cookie cookie = new Cookie(JwtUtil.AUTHORIZATION_HEADER, googleToken);
         cookie.setPath("/");
@@ -82,7 +86,38 @@ public class UserController {
         cookie.setSecure(true);
         httpServletResponse.addCookie(cookie);
 
-        BaseResponse<String> baseResponse = new BaseResponse<>(BaseResponseStatus.GOOGLE_LOGIN_SUCCESS, googleToken);
-        return ResponseEntity.status(HttpStatus.OK).body(baseResponse);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(new URI("https://www.giftipie.me/"));
+        return new ResponseEntity<>(headers, HttpStatus.FOUND);
     }
+
+//    // 유저 테스트 시 적용 예정
+//    @GetMapping("/kakao/callback")
+//    public ResponseEntity<BaseResponse<String>> kakaoLogin(@RequestParam String code,
+//                                                           HttpServletResponse httpServletResponse) throws JsonProcessingException, UnsupportedEncodingException {
+//        String kakaoToken = kakaoService.kakaoLogin(code);
+//        Cookie cookie = new Cookie(JwtUtil.AUTHORIZATION_HEADER, kakaoToken);
+//        cookie.setPath("/");
+//        cookie.setHttpOnly(true);
+//        cookie.setSecure(true);
+//        httpServletResponse.addCookie(cookie);
+//
+//        BaseResponse<String> baseResponse = new BaseResponse<>(BaseResponseStatus.KAKAO_LOGIN_SUCCESS, kakaoToken);
+//        return ResponseEntity.status(HttpStatus.OK).body(baseResponse);
+//    }
+
+//    // 유저 테스트 시 적용 예정
+//    @GetMapping("/login/oauth2/code/google")
+//    public ResponseEntity<BaseResponse<String>> googleLogin(@RequestParam String code,
+//                                                            HttpServletResponse httpServletResponse) throws JsonProcessingException, UnsupportedEncodingException {
+//        String googleToken = googleService.googleLogin(code);
+//        Cookie cookie = new Cookie(JwtUtil.AUTHORIZATION_HEADER, googleToken);
+//        cookie.setPath("/");
+//        cookie.setHttpOnly(true);
+//        cookie.setSecure(true);
+//        httpServletResponse.addCookie(cookie);
+//
+//        BaseResponse<String> baseResponse = new BaseResponse<>(BaseResponseStatus.GOOGLE_LOGIN_SUCCESS, googleToken);
+//        return ResponseEntity.status(HttpStatus.OK).body(baseResponse);
+//    }
 }
