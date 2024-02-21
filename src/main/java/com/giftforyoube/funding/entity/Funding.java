30acef103dbf509bcd0f93058e84e21edc10d9d7
DoKkangs <snnam0421@naver.com>
2024-02-21 16:53:01 +0900

    feat : Funding 엔티티 Donation Cascade 적용

--- src/main/java/com/giftforyoube/funding/entity/Funding.java
+++ src/main/java/com/giftforyoube/funding/entity/Funding.java
@@ -46,7 +46,7 @@ public class Funding extends Auditable implements Serializable {
     @JoinColumn(name = "user_id")
     private User user;
 
-    @OneToMany(mappedBy = "funding", fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "funding", fetch = FetchType.LAZY,cascade = CascadeType.ALL)
     private List<Donation> donations = new ArrayList<>();
 
     @Builder
