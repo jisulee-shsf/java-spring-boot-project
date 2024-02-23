ed9f4e8224bf6001f7926b319bd01e46d15d4b66
hyun <kingmandoo95@gmail.com>
2024-02-23 20:53:09 +0900

    feat : authenticationCode 지역변수로 변경 + 난수생성, 전송메서드 병합

--- src/main/java/com/giftforyoube/notification/service/MailingService.java
+++ src/main/java/com/giftforyoube/notification/service/MailingService.java
@@ -5,6 +5,7 @@ import jakarta.mail.MessagingException;
 import jakarta.mail.internet.InternetAddress;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
@@ -21,9 +22,10 @@ public class MailingService {
 
     private final JavaMailSender javaMailSender;
     private final SpringTemplateEngine templateEngine;
-    private static final String senderEmail = "wamandoo95@gmail.com";
-    private static int authenticationCode;
     private static final String EMAIL_TITLE_PREFIX = "[Giftipie] ";
+
+    @Value("${spring.mail.username}")
+    private String senderEmail;
 
     @Async
     public void sendNotificationEmail(Notification notification) throws MessagingException {
@@ -63,43 +65,43 @@ public class MailingService {
         javaMailSender.send(message);
     }
 
-    // 랜덤 숫자 생성
-    public static void createCode() {
-        Random random = new Random();
-        authenticationCode = random.nextInt(9000) + 1000; // 1000에서 9999 사이 랜덤숫자
-    }
+    ////////////////////////////////////////////////////////////////
 
     // 회원가입 검증 이메일 생성
-    public MimeMessage createMail(String mail) {
-        createCode();
+    public MimeMessage createMail(String mail, int authenticationCode) throws MessagingException {
         MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
 
-        try {
-            // 이메일 주소 형식 검증
-            InternetAddress emailAddress = new InternetAddress(mail);
-            emailAddress.validate();
+        // 이메일 주소 형식 검증
+        InternetAddress emailAddress = new InternetAddress(mail);
+        emailAddress.validate();
 
-            message.setFrom(senderEmail);
-            message.setRecipients(MimeMessage.RecipientType.TO, String.valueOf(emailAddress));
-            message.setSubject(EMAIL_TITLE_PREFIX + "이메일 인증 코드입니다.");
-            String body = "";
-            body += "<h3>" + "안녕하세요. 기프티파이입니다." + "</h3>";
-            body += "<h3>" + "요청하신 인증 번호 입니다." + "</h3>";
-            body += "<h1>" + authenticationCode + "</h1>";
-            body += "<h3>" + "감사합니다!" + "</h3>";
-            message.setText(body, "UTF-8", "html");
-
-        } catch (MessagingException e) {
-            throw new RuntimeException(e);
-        }
+        // 메일 세팅
+        helper.setFrom(senderEmail);
+        helper.setTo(mail);
+        helper.setSubject(EMAIL_TITLE_PREFIX + "이메일 인증 코드입니다.");
+        String body = "";
+        body += "<h3>" + "안녕하세요. 기프티파이입니다." + "</h3>";
+        body += "<h3>" + "요청하신 인증 번호 입니다." + "</h3>";
+        body += "<h1>" + authenticationCode + "</h1>";
+        body += "<h3>" + "감사합니다!" + "</h3>";
+        helper.setText(body, true);
 
         return message;
     }
 
-    // 회원가입 이메일 검증 메일 전송
+    // 회원가입 이메일 검증 메일 전송한 후, 인증코드 반환
     public int sendMail(String mail) {
-        MimeMessage message = createMail(mail);
-        javaMailSender.send(message);
+        Random random = new Random();
+        int authenticationCode = random.nextInt(9000) + 1000; // 1000에서 9999 사이 랜덤 숫자 생성
+
+        try {
+            // 메일 생성 및 전송
+            MimeMessage message = createMail(mail, authenticationCode);
+            javaMailSender.send(message);
+        } catch (MessagingException e) {
+            throw new RuntimeException("이메일 전송 중 오류가 발생했습니다.", e);
+        }
 
         return authenticationCode;
     }
